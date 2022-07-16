0a89f5fcc05b694d062832ea3cb1fb9f656d8031
changjiu.wang <3158269981@qq.com>
2022-07-16 10:25:16 +0800

    优化解析

--- src/main/java/com/rrtv/parser/LimitSQLParser.java
+++ src/main/java/com/rrtv/parser/LimitSQLParser.java
@@ -1,8 +1,10 @@
 package com.rrtv.parser;
 
+import com.rrtv.exception.SqlParserException;
 import com.rrtv.parser.data.LimitData;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.JdbcNamedParameter;
+import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.statement.select.Limit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,15 +19,26 @@ public class LimitSQLParser {
         if (Objects.nonNull(limit)) {
             Expression offset = limit.getOffset();
             Expression rowCount = limit.getRowCount();
-            if (offset instanceof JdbcNamedParameter && rowCount instanceof JdbcNamedParameter) {
-                String offsetValue = JdbcNamedParameter.class.cast(offset).getName();
-                String rowCountValue = JdbcNamedParameter.class.cast(rowCount).getName();
-                return new LimitData(Integer.valueOf(offsetValue), Integer.valueOf(rowCountValue));
+            Integer offsetValue = 0;
+            Integer rowCountValue = 0;
+            if(offset instanceof JdbcNamedParameter){
+                String offsetValueString = JdbcNamedParameter.class.cast(offset).getName();
+                offsetValue = Integer.valueOf(offsetValue);
+            } else if(offset instanceof LongValue){
+                offsetValue = LongValue.class.cast(offset).getBigIntegerValue().intValue();
             } else {
-                if(logger.isWarnEnabled()){
-                    logger.warn("offset 或者 rowCount 不是 JdbcNamedParameter 实例，放弃解析 Limit");
-                }
+                throw new SqlParserException("无法解析Limit中offset参数");
             }
+
+            if (rowCount instanceof JdbcNamedParameter) {
+                String rowCountValueString = JdbcNamedParameter.class.cast(rowCount).getName();
+                rowCountValue = Integer.valueOf(rowCountValueString);
+            } else if(rowCount instanceof LongValue){
+                rowCountValue = LongValue.class.cast(rowCount).getBigIntegerValue().intValue();
+            } else {
+                throw new SqlParserException("无法解析Limit中rowCount参数");
+            }
+            return new LimitData(offsetValue, rowCountValue);
         }
         return null;
     }
